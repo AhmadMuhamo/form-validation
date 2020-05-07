e4499291af258e82193a05b3f83acf45139fc441
Ahmad Ibrahim <ahmad.ibrahim@watheqsys.com>
2020-05-08 01:43:55 +0200

    Restructure & Refactor

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log('app started')
+
+const config = {
+  API_URL: 'https://private-b2e6827-robustatask.apiary-mock.com',
+  API_PATH_SIGNUP: '/auth/register',
+  API_PATH_SIGNIN: '/auth/login',
+}
--- script.js
+++
@@ -1,3 +0,0 @@
-var API_URL = 'https://private-b2e6827-robustatask.apiary-mock.com';
-var API_PATH_SIGNUP = '/auth/register';
-var API_PATH_SIGNIN = '/auth/login';
