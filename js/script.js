c09673c6ce40691ff783cc8176457f1ddcee8389
Ahmad Ibrahim <ahmad.ibrahim@watheqsys.com>
2020-05-09 00:54:24 +0200

    Form Handler function

--- js/script.js
+++ js/script.js
@@ -8,6 +8,12 @@ const config = {
 
 const navLinks = [...document.querySelectorAll('.nav-link')]
 const form = document.getElementById('signup-form')
+const formFields = {
+    name: document.getElementById('name'),
+    email: document.getElementById('email'),
+    username: document.getElementById('username'),
+    password: document.getElementById('password')
+}
 
 navLinks.forEach((link) => {
   link.addEventListener('click', () => {
@@ -17,29 +23,47 @@ navLinks.forEach((link) => {
   })
 })
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  formHandler({ name: 'test' }, 'signup')
+})
+
 const switchTab = (tab) => {
-    const signupOnlyfields = [...document.querySelectorAll('.signup-only')]
+  const signupOnlyfields = [...document.querySelectorAll('.signup-only')]
+  const signupNavLink = document.getElementById('to-signup')
+  const loginNavLink = document.getElementById('to-login')
+  const submitBtn = document.getElementById('submit-btn')
   if (tab === 'signup') {
-    document.getElementById('to-signup').classList.add('active')
-    document.getElementById('to-login').classList.remove('active')
-    document.getElementById('password').autocomplete = 'new-password'
-    document.getElementById('submit-btn').textContent = 'Signup'
+    signupNavLink.classList.add('active')
+    loginNavLink.classList.remove('active')
+    formFields.password.autocomplete = 'new-password'
+    submitBtn.textContent = 'Signup'
     signupOnlyfields.forEach((field) => {
-        field.style.display = 'block'
-        field.getElementsByTagName('input')[0].required = true
+      field.style.display = 'block'
+      field.getElementsByTagName('input')[0].required = true
     })
   } else {
-    document.getElementById('to-login').classList.add('active')
-    document.getElementById('to-signup').classList.remove('active')
-    document.getElementById('password').autocomplete = 'current-password'
-    document.getElementById('submit-btn').textContent = 'Login'
+    loginNavLink.classList.add('active')
+    signupNavLink.classList.remove('active')
+    formFields.password.autocomplete = 'current-password'
+    submitBtn.textContent = 'Login'
     signupOnlyfields.forEach((field) => {
-        field.style.display = 'none'
-        field.getElementsByTagName('input')[0].required = false
+      field.style.display = 'none'
+      field.getElementsByTagName('input')[0].required = false
     })
   }
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-})
+const formHandler = async (payload, type) => {
+  const url = `${config.API_URL}${
+    type === 'signup' ? config.API_PATH_SIGNUP : config.API_PATH_SIGNIN
+  }`
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+  const data = await response.json()
+}
