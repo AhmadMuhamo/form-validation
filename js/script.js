22ff61cc018dee3ea8180af1ee4af9ee3c43c986
Ahmad Ibrahim <ahmad.ibrahim@watheqsys.com>
2020-05-09 02:20:07 +0200

    Form Validation

--- js/script.js
+++ js/script.js
@@ -8,11 +8,19 @@ const config = {
 
 const navLinks = [...document.querySelectorAll('.nav-link')]
 const form = document.getElementById('signup-form')
+
 const formFields = {
-    name: document.getElementById('name'),
-    email: document.getElementById('email'),
-    username: document.getElementById('username'),
-    password: document.getElementById('password')
+  name: document.getElementById('name'),
+  email: document.getElementById('email'),
+  username: document.getElementById('username'),
+  password: document.getElementById('password'),
+}
+
+let validation = {
+  name: false,
+  email: false,
+  username: false,
+  password: false,
 }
 
 navLinks.forEach((link) => {
@@ -25,15 +33,116 @@ navLinks.forEach((link) => {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  formHandler({ name: 'test' }, 'signup')
+  const requestObject = {}
+  if (form.dataset.type === 'signup') {
+    if (
+      validation.name &&
+      validation.email &&
+      validation.username &&
+      validation.password
+    ) {
+      requestObject.name = formFields.name.value
+      requestObject.email = formFields.email.value
+      requestObject.username = formFields.username.value
+      requestObject.password = formFields.password.value
+      formHandler(requestObject, 'signup')
+    }
+  } else {
+    requestObject.email = formFields.email.value
+    requestObject.password = formFields.password.value
+    if (validation.email && validation.password) {
+      formHandler(requestObject, 'login')
+    }
+  }
 })
 
+/**
+ * Fields validation
+ */
+for (field in formFields) {
+  const input = formFields[field]
+  const formGroup = input.parentElement
+  let errorSpan = formGroup.getElementsByTagName('span')[0]
+  input.addEventListener('blur', () => {
+    if (!input.value) {
+      formGroup.classList.add('has-error')
+      errorSpan.textContent = `The ${input.id} field is required.`
+      validation[`${input.id}`] = false
+    }
+  })
+  switch (input.id) {
+    case 'name':
+      input.addEventListener('change', () => {
+        if (input.value) {
+          formGroup.classList.remove('has-error')
+          errorSpan.textContent = ''
+          validation[`${input.id}`] = true
+        }
+      })
+      break
+    case 'email':
+      const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      input.addEventListener('change', () => {
+        let valid = emailRegex.test(input.value)
+        if (!valid) {
+          formGroup.classList.add('has-error')
+          errorSpan.textContent = 'The email is not valid.'
+          validation[`${input.id}`] = false
+        } else {
+          formGroup.classList.remove('has-error')
+          errorSpan.textContent = ''
+          validation[`${input.id}`] = true
+        }
+      })
+      break
+    case 'username':
+      input.addEventListener('change', () => {
+        if (input.value.length < 5) {
+          formGroup.classList.add('has-error')
+          errorSpan.textContent = 'Username must contain at least 4 characters.'
+          validation[`${input.id}`] = false
+        } else {
+          formGroup.classList.remove('has-error')
+          errorSpan.textContent = ''
+          validation[`${input.id}`] = true
+        }
+      })
+      break
+    case 'password':
+      const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+      input.addEventListener('change', () => {
+        let valid = passwordRegex.test(input.value)
+        if (!valid) {
+          formGroup.classList.add('has-error')
+          errorSpan.textContent =
+            'Password must contain 8 characters or more and at least one number.'
+          validation[`${input.id}`] = false
+        } else {
+          formGroup.classList.remove('has-error')
+          errorSpan.textContent = ''
+          validation[`${input.id}`] = true
+        }
+      })
+      break
+    default:
+      break
+  }
+}
+
+/**
+ * Switching between tabs
+ * @param {*} tab, tab to navigate to
+ */
 const switchTab = (tab) => {
   const signupOnlyfields = [...document.querySelectorAll('.signup-only')]
   const signupNavLink = document.getElementById('to-signup')
   const loginNavLink = document.getElementById('to-login')
   const submitBtn = document.getElementById('submit-btn')
+
+  initializeTab()
+
   if (tab === 'signup') {
+    form.dataset.type = 'signup'
     signupNavLink.classList.add('active')
     loginNavLink.classList.remove('active')
     formFields.password.autocomplete = 'new-password'
@@ -43,6 +152,7 @@ const switchTab = (tab) => {
       field.getElementsByTagName('input')[0].required = true
     })
   } else {
+    form.dataset.type = 'login'
     loginNavLink.classList.add('active')
     signupNavLink.classList.remove('active')
     formFields.password.autocomplete = 'current-password'
@@ -54,6 +164,25 @@ const switchTab = (tab) => {
   }
 }
 
+/**
+ * Reset tab on switch
+ */
+const initializeTab = () => {
+  for (field in formFields) {
+    const input = formFields[field]
+    const formGroup = input.parentElement
+
+    input.value = ''
+    formGroup.classList.remove('has-error')
+    formGroup.getElementsByTagName('span')[0].textContent = ''
+  }
+}
+
+/**
+ * Async form handler, API Call
+ * @param {Object} payload, Form data
+ * @param {String} type, Signup or signin
+ */
 const formHandler = async (payload, type) => {
   const url = `${config.API_URL}${
     type === 'signup' ? config.API_PATH_SIGNUP : config.API_PATH_SIGNIN
